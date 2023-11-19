a46c0468301d2cf40d3594632062c0abc94e9f72
Nicholas <nicholassinaga@hotmail.com>
2023-11-19 18:43:07 +0700

    Progres Sesi Ke 3

---
+++ looping.js
@@ -0,0 +1,61 @@
+// While ()
+// for ()
+
+
+// dalam looping ada yang namanya kiri atau kanan dimana kiri = pengurangan dan kanan = pertambahan
+
+//contoh looping while ke arah kanan
+// let i = 5;
+
+// while (i>=0) {
+//     console.log(i)
+//     i--
+// }
+
+// //contoh looping while ke arah kiri
+//  let q = 5;
+
+//  while (q>=0) {
+//     console.log(q)
+//     q--
+//  }
+
+ // looping menggunakan while bisa terjebak ke dalam infinite loop
+ //contoh infinite loop
+
+ /*
+ let w = 5;
+
+ while (w>=5) {
+    console.log(w);
+    w++
+ }
+ */
+
+//for loop
+
+//  for (let i = 0; i <= 100; i++) {
+//     console.log(i);
+//  }
+
+
+//method lengt untuk menghitung karakter pada string atau elemen pada array
+
+// let name = "nicholas";
+
+// for (let i = 0; i < name.length; i++) {
+//     console.log(name[i]);
+// }
+
+//diberikan sebuah kasus untuk mereverse sebuah string tanpa menggunakan method reverse
+
+// let name = "Nicholas"
+// let result = "";
+
+// for(let i = name.length - 1; i>=0; i--) {
+//     result += name[i]
+// }
+
+// console.log(result);
+
+//nested loop
